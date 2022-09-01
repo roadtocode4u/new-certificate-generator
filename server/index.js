f971a7c9891c7c64f4796e7d4f09045e99810ba5
prajaktadharpure28 <prajaktadharpure28@gmail.com>
2022-09-01 09:47:15 +0530

    Added schema and models

--- server/index.js
+++ server/index.js
@@ -4,13 +4,114 @@ const bodyParser = require('body-parser');
 const path = require('path'); 
 const mongoose = require('mongoose')
 
+const healthCheck = require("./models/health");
+const User = require("./models/User");
+const Proof = require("./models/Proof");
+const Certificate = require("./models/Certificate");
+
 const app = express();
 app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
   console.log('Connected to DB 📦');
+});
+
+app.get('/health', async (req, res) => {
+  const health = await healthCheck.find();
+  res.send({
+    status: "success",
+    message: "Health check found",
+    data: health,
+  });
+});
+
+app.get('/get/health', async (req, res) => {
+  const health = await healthCheck.find();
+  res.send({
+    status: "success",
+    message: "Health check found",
+    data: health,
+  });
+});
+
+app.post('/add/user', async (req, res) => {
+  const user = new User({
+    fullName: req.body.fullName,
+    email: req.body.email,
+  });
+  await user.save();
+  res.send({
+    status: "success",
+    message: "User added successfully",
+  });
+});
+
+app.get('/get/user', async (req, res) => {
+  const user = await User.find();
+  res.send({
+    status: "success",
+    message: "User found",
+  });
+
+});
+
+
+
+app.post('/add/certificate', async (req, res) => {
+  const certificate = new Certificate({
+    student_name: req.body.student_name,
+    student_id: req.body.student_id,
+    student_email: req.body.student_email,
+    course_name: req.body.course_name,
+    starting_date: req.body.starting_date,
+    ending_date: req.body.ending_date,
+    certificate_id: req.body.certificate_id,
+    certificate_url: req.body.certificate_url,
+    proof_work: req.body.proof_work
+  });
+
+  await certificate.save();
+  res.send({
+    status: "success",
+    message: "Certificate added successfully"
+  });
+});
+
+app.get('get/certificate', async (req, res) => {
+  const certificates = await Certificate.find();
+  res.send({
+    status: "success",
+    message: "Certificates found",
+    data: certificates,
+  });
+});
+
+app.post('/add/proof', async (req, res) => {
+  const proof = new Proof({
+    title: req.body.title,
+    description: req.body.description,
+    link: req.body.link,
+    date: req.body.date,
+    technology: req.body.technology,
+    visibility: req.body.visibility,
+    user: req.body.user,
+  });
+  await proof.save();
+  res.send({
+    status: "success",
+    message: "Proof added successfully",
+  });
+});
+
+app.get('/get/proof', async (req, res) => {
+  const proof = await Proof.find();
+  res.send({
+    status: "success",
+    message: "Proof found",
+    data: proof,
+  });
 });
 
 if(process.env.NODE_ENV === 'production') {
